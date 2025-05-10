a828d5cc14d40ec69f5298ca832453a3d8cfab70
Hamza Maach <hamzamaach56@gmail.com>
2025-05-10 19:24:08 +0100

    make sure it is abs to get it's exp()

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -2,7 +2,7 @@ pub fn nbr_function(c: i32) -> (i32, f64, f64) {
     let e: f64 = if c >= 0 {
         c.pow(c as u32) as f64
     } else {
-        f64::NAN
+        c.abs().pow(c.abs() as u32) as f64
     };
 
     let natural_logarithm_absolute_v: f64 = (c as f64).abs().ln();
