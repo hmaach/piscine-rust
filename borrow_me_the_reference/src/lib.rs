df0b9abcb64c79e37695c66be0735e2425dc4c5f
Hamza Maach <hamzamaach56@gmail.com>
2025-05-11 07:41:41 +0100

    borrow_me_the_reference first attempt

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,7 +1,29 @@
 pub fn delete_and_backspace(s: &mut String) {
+    let mut i = 0;
+    let mut to_delete = 0;
+    let mut to_backspace = 0;
 
+    while i < s.clone().len() {
+        if s.chars().nth(i).unwrap() == '+' {
+            to_delete += 1;
+            s.replace_range(i..i + 1, "");
+        } else if s.chars().nth(i).unwrap() == '-' {
+            to_backspace += 1;
+            s.replace_range(i..i + 1, "");
+        } else if s.chars().nth(i).unwrap() != '+' && to_delete > 0 {
+            s.replace_range(i..i + to_delete, "");
+            to_delete = 0;
+            // i -= to_delete;
+        } else if s.chars().nth(i).unwrap() != '-' && to_backspace > 0 {
+            s.replace_range(i - to_backspace..i, "");
+            to_backspace = 0;
+            // i += to_backspace;
+        } else {
+            i += 1;
+        }
+    }
+    // for (i, ch) in s.clone().chars().enumerate() {
+    // }
 }
 
-pub fn do_operations(v: &mut [String]) {
-    
-}
+pub fn do_operations(v: &mut [String]) {}
