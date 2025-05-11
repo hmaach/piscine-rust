0a5115a3f30ac5e02ace832c512f608973b1ce7b
Hamza Maach <hamzamaach56@gmail.com>
2025-05-11 09:39:45 +0100

    borrow_me_the_reference

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -17,7 +17,7 @@ pub fn delete_and_backspace(s: &mut String) {
         } else if s.chars().nth(i).unwrap() != '-' && to_backspace > 0 {
             s.replace_range(i - to_backspace..i, "");
             to_backspace = 0;
-            // i += to_backspace;
+        //     // i += to_backspace;
         } else {
             i += 1;
         }
@@ -26,4 +26,23 @@ pub fn delete_and_backspace(s: &mut String) {
     // }
 }
 
-pub fn do_operations(v: &mut [String]) {}
+pub fn do_operations(v: &mut [String]) {
+    let mut sum = 0;
+    let mut cur_op = '+';
+    let mut to_save = String::new();
+    for (i, nb_str) in v.iter().enumerate() {
+        for ch in nb_str.chars() {
+            dbg!(sum);
+            if ch == '+' {
+                sum += to_save.parse::<i32>().unwrap();
+                cur_op = '+';
+            } else if ch == '-' {
+                sum -= to_save.parse::<i32>().unwrap();
+                cur_op = '-';
+            } else {
+                to_save.push(ch);
+            }
+        }
+        sum = 0;
+    }
+}
