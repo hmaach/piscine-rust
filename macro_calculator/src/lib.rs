cdbcfecaee5e523eb32409bbccb6e3e7c5bd883a
Hamza Maach <hamzamaach56@gmail.com>
2025-05-17 16:32:02 +0100

    calculate macros

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -13,19 +13,35 @@ pub struct Food {
 fn round(f: f64) -> f64 {
     (f * 100.0).round() / 100.0
 }
+fn format_float(value: f64) -> String {
+    let rounded = (value * 100.0).round() / 100.0;
+    if (rounded * 10.0).round() / 10.0 == rounded {
+        format!("{:.1}", rounded)
+    } else {
+        format!("{:.2}", rounded)
+    }
+}
 
 pub fn calculate_macros(foods: &[Food]) -> json::JsonValue {
-    let mut data = HashMap::new();
+    let mut data: HashMap<&'static str, f64> = HashMap::new();
+    let mut data1 = HashMap::new();
+
+    if foods.len() == 0 {
+        return json::JsonValue::from(data);
+    }
 
     for f in foods {
         let calories = &f.calories.1.replace("kcal", "").parse::<f64>().unwrap();
-        *data.entry("calories").or_insert(0.) += round(calories * f.nbr_of_portions);
-        *data.entry("fats").or_insert(0.) += round(f.fats * f.nbr_of_portions);
-        *data.entry("carbs").or_insert(0.) += round(f.carbs * f.nbr_of_portions);
-        *data.entry("proteins").or_insert(0.) += round(f.proteins * f.nbr_of_portions);
+        *data.entry("calories").or_insert(0.) += calories * f.nbr_of_portions;
+        *data.entry("fats").or_insert(0.) += f.fats * f.nbr_of_portions;
+        *data.entry("carbs").or_insert(0.) += f.carbs * f.nbr_of_portions;
+        *data.entry("proteins").or_insert(0.) += f.proteins * f.nbr_of_portions;
     }
 
-    *data.entry("calories").or_insert(0.) = round(*data.get("calories").unwrap());
+    data1.insert("calories", format_float(*data.get("calories").unwrap()));
+    data1.insert("fats", format_float(*data.get("fats").unwrap()));
+    data1.insert("carbs", format_float(*data.get("carbs").unwrap()));
+    data1.insert("proteins", format_float(*data.get("proteins").unwrap()));
 
-    json::JsonValue::from(data)
+    json::JsonValue::from(data1)
 }
