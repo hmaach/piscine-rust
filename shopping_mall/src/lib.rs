6398b4e19add8a0519e029593e6212a319c77eec
Hamza Maach <hamzamaach56@gmail.com>
2025-05-17 18:53:25 +0100

    tmjnin w7a9rb

---
+++ shopping_mall/src/lib.rs
@@ -0,0 +1,94 @@
+mod mall;
+
+// use std::iter::Map;
+
+pub use mall::*;
+
+pub fn biggest_store(mall: &Mall) -> (String, Store) {
+    let mut biggest: u64 = 0;
+    let mut res_store: (Option<String>, Option<Store>) = (None, None);
+
+    for (_, floor) in &mall.floors {
+        for (key, store) in &floor.stores {
+            if store.square_meters > biggest {
+                biggest = store.square_meters;
+                res_store = (Some(key.to_string()), Some(store.clone()));
+            }
+        }
+    }
+
+    (res_store.0.unwrap(), res_store.1.unwrap())
+}
+
+pub fn highest_paid_employee(mall: &Mall) -> (String, Employee) {
+    let mut biggest: f64 = 0.;
+    let mut res_employee: (Option<String>, Option<Employee>) = (None, None);
+
+    for (_, floor) in &mall.floors {
+        for (_, store) in &floor.stores {
+            for (key, employee) in store.employees.clone() {
+                if employee.salary > biggest {
+                    biggest = employee.salary;
+                    res_employee = (Some(key.to_string()), Some(employee.clone()));
+                }
+            }
+        }
+    }
+
+    (res_employee.0.unwrap(), res_employee.1.unwrap())
+}
+
+pub fn nbr_of_employees(mall: &Mall) -> usize {
+    let mut counter: usize = 0;
+
+    for (_, _) in mall.guards.clone() {
+        counter += 1;
+    }
+
+    for (_, floor) in &mall.floors {
+        for (_, store) in &floor.stores {
+            for (_, _) in store.employees.clone() {
+                counter += 1;
+            }
+        }
+    }
+    counter
+}
+pub fn check_for_securities(mall: &mut Mall, guards: Vec<(String, Guard)>) {
+    let mut total_size: u64 = 0;
+    let mut total_guards: u64 = 0;
+
+    for (_, _) in mall.guards.clone() {
+        total_guards += 1;
+    }
+
+    for (_, floor) in &mall.floors {
+        for (_, store) in &floor.stores {
+            total_size += store.square_meters;
+        }
+    }
+
+    let required_guards = (total_size as f64 / 200.).ceil() as u64;
+    let guards_to_add = (required_guards - total_guards) as usize;
+
+    let mut i: usize = 0;
+    while i < guards_to_add {
+        mall.hire_guard(guards[i].clone().0, guards[i].1);
+        i += 1;
+    }
+}
+pub fn cut_or_raise(mall: &mut Mall) {
+    for (_, floor) in &mut mall.floors {
+        for (_, store) in &mut floor.stores {
+            for (_, employee) in &mut store.employees {
+                dbg!(employee.working_hours.1 - employee.working_hours.0);
+                if employee.working_hours.1 - employee.working_hours.0 >= 10 {
+                    employee.raise(employee.salary * 0.10);
+                    dbg!(employee.salary);
+                } else {
+                    employee.cut(employee.salary * 0.10);
+                }
+            }
+        }
+    }
+}
