2d136f22c89dd82f85474e3429c9e034a189638f
Hamza Maach <hamzamaach56@gmail.com>
2025-05-18 18:00:55 +0100

    talk with the computer

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -6,7 +6,7 @@ enum Answer {
 }
 
 pub fn talking(text: &str) -> &str {
-    if text.len() == 0 {
+    if text.to_string().trim().len() == 0 {
         return "Just say something!";
     }
 
@@ -14,7 +14,11 @@ pub fn talking(text: &str) -> &str {
     let mut is_yelling = true;
     let mut not_yelling = false;
     let mut is_question = false;
+    let all_alphabitics = text.to_string().chars().any(|c| c.is_alphabetic());
 
+    if !all_alphabitics {
+        return "Sure.";
+    }
     if text.to_string().chars().last().unwrap() == '?' {
         is_question = true;
     }
@@ -27,11 +31,11 @@ pub fn talking(text: &str) -> &str {
         }
     }
 
-    if is_yelling && !is_question {
+    if is_yelling && !is_question && all_alphabitics {
         talk_type = Answer::Yelling;
-    } else if is_yelling && is_question {
+    } else if is_yelling && is_question && all_alphabitics {
         talk_type = Answer::YellingQuestion;
-    } else if not_yelling {
+    } else if not_yelling && is_question {
         talk_type = Answer::NotYelling;
     } else {
         talk_type = Answer::Default;
