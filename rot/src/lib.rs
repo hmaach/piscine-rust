938d08c670ee9eac02845a2ec759a1644ba7eabc
Hamza Maach <hamzamaach56@gmail.com>
2025-05-19 10:42:26 +0100

    The rotational cipher "ROT13

--- rot/src/lib.rs
+++ rot/src/lib.rs
@@ -1,20 +1,48 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    let mut step = key;
-    if key > 26 {
-        step = key % 26
+    let mut res = String::new();
+
+    for ch in input.to_string().chars() {
+        let new_ch = match ch {
+            'a'..='z' => {
+                let mut step = 0;
+                if key >= 0 {
+                    if (((ch as u8) + ((key % 26) as u8)) as char) > 'z' {
+                        step = (ch as u8) - (('z' as u8) - ('a' as u8) + 1) + ((key % 26) as u8);
+                    } else {
+                        step = (ch as u8) + ((key % 26) as u8);
+                    }
+                } else {
+                    if (((ch as u8) - ((key.abs() % 26) as u8)) as char) < 'a' {
+                        step =
+                            (ch as u8) + (('z' as u8) - ('a' as u8) + 1) - ((key.abs() % 26) as u8);
+                    } else {
+                        step = (ch as u8) - ((key.abs() % 26) as u8);
+                    }
+                }
+                step as char
+            }
+            'A'..='Z' => {
+                let mut step = 0;
+                if key >= 0 {
+                    if (((ch as u8) + ((key % 26) as u8)) as char) > 'Z' {
+                        step = (ch as u8) - (('Z' as u8) - ('A' as u8) + 1) + ((key % 26) as u8);
+                    } else {
+                        step = (ch as u8) + ((key % 26) as u8);
+                    }
+                } else {
+                    if (((ch as u8) - ((key.abs() % 26) as u8)) as char) < 'A' {
+                        step =
+                            (ch as u8) + (('Z' as u8) - ('A' as u8) + 1) - ((key.abs() % 26) as u8);
+                    } else {
+                        step = (ch as u8) - ((key.abs() % 26) as u8);
+                    }
+                }
+                step as char
+            }
+            _ => ch,
+        };
+
+        res.push(new_ch);
     }
-
-    dbg!((('S' as u8) + (5 % 26) as u8) as char);
-
-    input
-        .to_string()
-        .chars()
-        .map(|c| match c {
-            'a'..='m' => ((c as u8) + (key % 26) as u8) as char,
-            'n'..='z' => ((c as u8) - (key % 26) as u8) as char,
-            'A'..='M' => ((c as u8) + (key % 26) as u8) as char,
-            'N'..='Z' => ((c as u8) - (key % 26) as u8) as char,
-            _ => c,
-        })
-        .collect()
+    res
 }
