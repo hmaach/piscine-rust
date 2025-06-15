250d90e0ff5b061347a80bca82c48b1cb39e7572
Hamza Maach <hamzamaach56@gmail.com>
2025-06-15 13:57:11 +0100

    create a tic-tac-toe winner checker

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -1,9 +1,24 @@
-// pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
+pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
+    if diagonals('O', table) || horizontal('O', table) || vertical('O', table) {
+        return "player O won".to_owned();
+    }
+    if diagonals('X', table) || horizontal('X', table) || vertical('X', table) {
+        return "player X won".to_owned();
+    }
+    "tie".to_owned()
+}
 
-// }
+pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
+    if table[0][0] == player && table[1][1] == player && table[2][2] == player {
+        return true;
+    }
 
-// pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
-// }
+    if table[0][2] == player && table[1][1] == player && table[2][0] == player {
+        return true;
+    }
+
+    false
+}
 
 pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
     for row in table {
@@ -21,10 +36,10 @@ pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
 }
 
 pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
-    let mut row = 0;
     let mut col = 0;
-
+    
     while col < 3 {
+        let mut row = 0;
         let mut count = 0;
         while row < 3 {
             if table[row][col] == player {
