0103083689f4dcdad09e3db4f069d7a248c6605e
Hamza Maach <hamzamaach56@gmail.com>
2025-06-15 13:41:34 +0100

    check if a player have won vertictally

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -20,5 +20,23 @@ pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
     false
 }
 
-// pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
-// }
+pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
+    let mut row = 0;
+    let mut col = 0;
+
+    while col < 3 {
+        let mut count = 0;
+        while row < 3 {
+            if table[row][col] == player {
+                count += 1;
+            }
+            row += 1;
+        }
+
+        if count == 3 {
+            return true;
+        }
+        col += 1;
+    }
+    false
+}
