83bd841d82acee52e5e86abe59efe7eb665328ab
Hamza Maach <hamzamaach56@gmail.com>
2025-06-16 10:49:06 +0100

    insert into map and get its length

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut res: HashMap<&'a str, usize> = HashMap::new();
+    for word in words {
+        *res.entry(word).or_insert(0) += 1;
+    }
+    res
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.len()
+}
