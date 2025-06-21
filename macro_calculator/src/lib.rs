6a8dc4f5f66787711cd5a6478aa1ea23bf8765a7
Hamza Maach <hamzamaach56@gmail.com>
2025-06-21 12:56:59 +0100

    calculate calories in list of foods

---
+++ macro_calculator/src/lib.rs
@@ -0,0 +1,39 @@
+use json::JsonValue;
+
+pub struct Food {
+    pub name: String,
+    pub calories: (String, String),
+    pub fats: f64,
+    pub carbs: f64,
+    pub proteins: f64,
+    pub nbr_of_portions: f64,
+}
+
+pub fn calculate_macros(foods: &[Food]) -> JsonValue {
+    let mut cals: f64 = 0.;
+    let mut carbs: f64 = 0.;
+    let mut proteins: f64 = 0.;
+    let mut fats: f64 = 0.;
+
+    for food in foods {
+        cals += food
+            .calories
+            .1
+            .replace("kcal", "")
+            .parse::<f64>()
+            .unwrap_or(0.);
+        carbs += food.carbs;
+        fats += food.fats;
+        proteins += food.proteins * food.nbr_of_portions;
+    }
+
+    let round = |val: f64| (val * 100.0).round() / 100.0;
+
+    let mut res = json::Null;
+    res["cals"] = JsonValue::from(round(cals));
+    res["fats"] = JsonValue::from(round(fats));
+    res["carbs"] = JsonValue::from(round(carbs));
+    res["proteins"] = JsonValue::from(round(proteins));
+
+    return res;
+}
