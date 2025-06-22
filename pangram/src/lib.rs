5281a35196b1f23b6a033cd99c0e9a54703e6011
Hamza Maach <hamzamaach56@gmail.com>
2025-06-22 15:05:23 +0100

    check if a given string is pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,8 +3,9 @@ use std::collections::HashSet;
 pub fn is_pangram(s: &str) -> bool {
     let mut chars: HashSet<char> = HashSet::new();
     for ch in s.chars() {
-        if !chars.contains(&ch) && ch.is_alphabetic() {
-            chars.insert(ch);
+        let lower_ch = ch.to_ascii_lowercase();
+        if !chars.contains(&lower_ch) && lower_ch >= 'a' && lower_ch <= 'z' {
+            chars.insert(lower_ch);
         }
     }
 
