5401857f73567f65de221b3b52b09b3a93fbe9fe
Hamza Maach <hamzamaach56@gmail.com>
2025-06-22 16:30:14 +0100

    talking with your computer

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -6,21 +6,33 @@ enum TalkingType {
     Else,
 }
 
+pub fn all_uppercase(s: &str) -> bool {
+    let mut upper = 0;
+    for ch in s.chars() {
+        if ch >= 'A' && ch <= 'Z' {
+            upper += 1;
+        } else if ch >= 'a' && ch <= 'z' {
+            return false;
+        }
+    }
+    upper != 0
+}
+
 pub fn talking(text: &str) -> &str {
     let talking_type = match text {
-        s if s.chars().count() == 0 => TalkingType::Nothing,
+        s if s.trim().chars().count() == 0 => TalkingType::Nothing,
         s if s.chars().last().unwrap_or(' ') == '?' => {
-            if text.chars().any(|ch| ch.is_ascii_lowercase()) {
-                TalkingType::Ask
-            } else {
+            if all_uppercase(text) {
                 TalkingType::YellAsk
+            } else {
+                TalkingType::Ask
             }
         }
         _ => {
-            if text.chars().any(|ch| ch.is_ascii_lowercase()) {
-                TalkingType::Else
-            } else {
+            if all_uppercase(text) {
                 TalkingType::Yell
+            } else {
+                TalkingType::Else
             }
         }
     };
