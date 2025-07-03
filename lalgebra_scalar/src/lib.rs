6864933eff3b87f5e73270f0195d43770714abe2
Hamza Maach <hamzamaach56@gmail.com>
2025-07-03 13:00:02 +0100

    Define a data structure to represent a matrix of any size and implement some basic operations

--- lalgebra_scalar/src/lib.rs
+++ lalgebra_scalar/src/lib.rs
@@ -1,4 +1,6 @@
-pub trait Scalar: Sized {
+use std::ops::{Add, Div, Mul, Sub};
+
+pub trait Scalar: Clone + Add + Sub + Mul + Div {
     type Item;
     fn zero() -> Self::Item;
     fn one() -> Self::Item;
