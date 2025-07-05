98794e79f91970c31523fc095ced9e23d1c9b7af
Hamza Maach <hamzamaach56@gmail.com>
2025-07-05 10:08:04 +0100

    implement Person struct with lifetime

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub struct Person<'a> {
+    pub name: &'a str,
+    pub age: u8,
+}
+
+impl<'a> Person<'a> {
+    pub fn new(name: &'a str) -> Self {
+        Self { name, age: 0 }
+    }
+}
