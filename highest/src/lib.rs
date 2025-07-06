1cb6e409dd025d6f9d3ff5a54a34862baeae1e0c
Hamza Maach <hamzamaach56@gmail.com>
2025-07-06 14:04:25 +0100

    get latest, highest and highest_three of a list of u32

---
+++ highest/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+pub struct Numbers<'a> {
+    numbers: &'a [u32],
+}
+
+impl<'a> Numbers<'a> {
+    pub fn new(numbers: &'a [u32]) -> Self {
+        Self { numbers }
+    }
+
+    pub fn list(&self) -> &[u32] {
+        self.numbers
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.numbers.iter().last().copied()
+    }
+
+    pub fn highest(&self) -> Option<u32> {
+        self.numbers.iter().max().copied()
+    }
+
+    pub fn highest_three(&self) -> Vec<u32> {
+        let mut nums = self.numbers.to_vec();
+        nums.sort();
+        nums.reverse();
+        nums.truncate(3);
+        nums
+    }
+}
